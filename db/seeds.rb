ec483167bbcf1acd83851ac388d32acd3d08945a
irasrive <3mr.3omran@gmail.com>
2025-10-28 03:57:05 +0300

    CRUD & RESTful routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Article.create!([
+                  { title: "First Post", body: "For articles and blogs." },
+                  { title: "Exploring p5.js", body: "For p5.js exhibitions." }
+                ])
